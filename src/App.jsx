85c540084bea1350f7c58b71f25e5749bb817a3d
sandeep-6 <sandeep.saliganti@x-verity.com>
2025-05-17 10:02:04 +0530

    CI,CD Testing 1

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,11 @@ const App = () => {
   return (
     <div>
       <h1>CICD Pipeline -V4 is Done  leta_go</h1>
+      <h2>Bestinfra.App</h2>
+      <h3>Railway</h3>
     </div>
   )
 }
 
 export default App
+// import React from 'react'
