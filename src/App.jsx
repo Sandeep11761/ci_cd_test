3203a58e1845c588f3272a1b55253073dbd0750d
sandeep-6 <sandeep.saliganti@x-verity.com>
2025-05-17 14:39:02 +0530

    Saving 14

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,7 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-      <h1>CICD Pipeline -V4 is Done  leta_go</h1>
-      <h2>Bestinfra.App</h2>
-      <h3>Railway</h3>
+     <h1> Hello X-verity</h1>
     </div>
   )
 }
